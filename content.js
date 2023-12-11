3877ab5cdcaa006b25ca02f4f2bb1596003731d0
Y7T007 <yassir7t@gmail.com>
2023-12-11 03:31:24 +0100

    Refactor content.js and popup.js
    Update event listener in content.js and popup.js

--- content.js
+++ content.js
@@ -1,20 +1,8 @@
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.action === 'extractAndRedirect') {
-        // Extract the link from the script (modify this based on your criteria)
-            var scriptContainingLink = document.querySelector('script'); // Adjust this selector based on the actual structure of the page
-
-            var linkMatch = scriptContainingLink.textContent.match(/window\.location\.href\s*=\s*['"]([^'"]+)['"]/);
-
-            console.log('content :', scriptContainingLink.textContent );
-        if (linkMatch && linkMatch[1]) {
-            var extractedLink = linkMatch[1];
-
-            // Redirect to the extracted link
-            window.location.href = extractedLink;
-        } else {
-            console.log('Link not found in the script.');
-        }
-        }
+function (request, sender, sendResponse) {
+    if (request.action == "queryElement") {
+    var elementText = document.body.innerText;
+    sendResponse({ data: elementText });
     }
+}
 );
--- popup.js
+++ popup.js
@@ -1,28 +1,9 @@
-chrome.webRequest.onBeforeRedirect.addListener(
-    function(details) {
-        if (details.type === 'main_frame') {
-            // Check if Wi-Fi is turned on
-            chrome.system.network.getNetworkInterfaces(function(interfaces) {
-                var wifiConnected = interfaces.some(function(interface) {
-                    return interface.type === 'wifi' && interface.prefixLength > 0;
-                });
-
-                if (!wifiConnected) {
-                    // Wi-Fi is not connected, handle accordingly
-                    console.log('Wi-Fi is not connected');
-                    return;
-                }
-
-                // Wi-Fi is connected, but internet is not reachable
-                console.log('Internet connection is not available');
-
-                // You can add your logic here to handle the redirection
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    var activeTab = tabs[0];
-                    chrome.tabs.sendMessage(activeTab.id, { action: 'extractAndRedirect' });
-                });
-            });
-        }
-    },
-    { urls: ['<all_urls>'] }
-);
+document.addEventListener('DOMContentLoaded', function () {
+document.getElementById('queryButton').addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'queryElement' }, function (response) {
+        document.getElementById('result').innerText = response.data;
+    });
+    });
+});
+});
