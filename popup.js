0606d6876c290b278e8c474928029c8a9088af9a
Y7T007 <yassir7t@gmail.com>
2023-12-11 03:02:40 +0100

    Update popup.html and popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { action: 'extractAndRedirect' });
-});
+chrome.webRequest.onBeforeRedirect.addListener(
+    function(details) {
+        if (details.url === 'SPECIFIC_URL') {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                var activeTab = tabs[0];
+                chrome.tabs.sendMessage(activeTab.id, { action: 'extractAndRedirect' });
+            });
+        }
+    },
+    { urls: ['<all_urls>'] }
+);
