8b5af937b23f9ab1a9b912898992f31e25fd2604
Y7T007 <yassir7t@gmail.com>
2023-12-11 02:59:10 +0100

    Refactor content.js to extract and redirect link

--- content.js
+++ content.js
@@ -1,18 +1,18 @@
 chrome.runtime.onMessage.addListener(
 function (request, sender, sendResponse) {
-        if (request.action === 'extractAndRedirect') {
+    if (request.action === 'extractAndRedirect') {
+    // Extract the link from the script (modify this based on your criteria)
+    var scriptContainingLink = document.querySelector('script'); // Adjust this selector based on the actual structure of the page
+    var linkMatch = scriptContainingLink.textContent.match(/window\.location\.href\s*=\s*['"]([^'"]+)['"]/);
 
-            var scriptContainingLink = document.querySelector('script'); // Adjust this selector based on the actual structure of the page
-            var linkMatch = scriptContainingLink.textContent.match(/window\.location\.href\s*=\s*['"]([^'"]+)['"]/);
+    if (linkMatch && linkMatch[1]) {
+        var extractedLink = linkMatch[1];
 
-        if (linkMatch && linkMatch[1]) {
-            var extractedLink = linkMatch[1];
-
-            // Redirect to the extracted link
-            window.location.href = extractedLink;
-        } else {
-            console.log('Link not found in the script.');
-        }
-        }
+        // Redirect to the extracted link
+        window.location.href = extractedLink;
+    } else {
+        console.log('Link not found in the script.');
     }
+    }
+}
 );
